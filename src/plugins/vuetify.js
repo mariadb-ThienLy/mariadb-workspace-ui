a7942c1fea3668a9e49a7bd83283cb00bd4cd117
Thien Ly <thien.ly@mariadb.com>
2025-10-30 15:25:18 +0200

    Revert adding `stylesheetId` and `scope` options for Vuetify
    Revert the previous change because it causes styling issues with the
    `VOverlay` component, which is attached to the shadow DOM as a sibling
    of `VApp`.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -93,8 +93,6 @@ export default createVuetify({
   theme: {
     defaultTheme: 'vuetifyMxsTheme',
     themes: { vuetifyMxsTheme },
-    scope: '#mariadb-workspace',
-    stylesheetId: 'mariadb-workspace',
   },
   defaults: {
     VTextField: { variant: 'outlined', ...commonProps },
